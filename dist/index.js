c69598c5e9b32c269b2b0ed5a0ba8909cd31bc7f
Ashvary Gidian <ashvarygidian1996@gmail.com>
2024-06-22 17:07:15 +0530

    data fetched

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,30 @@
 "use strict";
-console.log("Hello World");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const userName = document.querySelector("#user");
+const form = document.querySelector("#form");
+const main_container = document.querySelector(".main-container");
+const myCustomFetcher = (url, options) => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield fetch(url, options);
+    const data = yield response.json();
+    //   console.log(data);
+    return data;
+});
+const showData = (user) => {
+};
+const fetchUser = (url) => {
+    myCustomFetcher(url, {}).then((users) => {
+        for (const user of users) {
+            showData(user);
+            console.log(user.id, user);
+        }
+    });
+};
+fetchUser("https://api.github.com/users");
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log("Hello World");
+const userName = document.querySelector("#user") as HTMLInputElement;
+const form = document.querySelector("#form") as HTMLFormElement;
+const main_container = document.querySelector(".main-container") as HTMLElement;
+
+interface UserData {
+  id: number;
+  login: string;
+  avatar_url: string;
+  url: string;
+}
+
+const myCustomFetcher = async <T>(
+  url: string,
+  options?: RequestInit
+): Promise<T> => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+  //   console.log(data);
+
+  return data;
+};
+const showData = (user: UserData) => {
+
+
+    
+};
+const fetchUser = (url: string) => {
+  myCustomFetcher<UserData[]>(url, {}).then((users) => {
+    for (const user of users) {
+      showData(user);
+      console.log(user.id, user);
+    }
+  });
+};
+
+fetchUser("https://api.github.com/users");
